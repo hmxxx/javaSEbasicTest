84c073ab7c7aa59f1bb7539da06a06fae9fb9306
hmx <2716089984@qq.com>
2019-08-02 19:54:49 +0800

    java练习

---
+++ Practice2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Practice2{
+     public static void main(String[] args){
+		 /*Scanner scanner = new Scanner(System.in);
+		 int score = scanner.nextInt();
+		 if(score>100|| score < 0){
+			 System.out.print("superMan");
+		 }else if(score >= 90&&score <=100){
+			 System.out.print("优秀");
+		 }else if(score >= 80 && score <= 90){
+			 System.out.print("良好");
+		 }else{
+			 System.out.print("不及格");
+		 }*/
+		 /*int i = 0;
+		 Scanner scanner = new Scanner(System.in);
+		 while(i != -1){
+			 System.out.print("请输入一个数：");
+		     i = scanner.nextInt();
+			 System.out.print("您输入的数为："+i);
+		 }*/
+		 int nums[] = {1,2,3,4,5,6};
+		 int nums2[] = null;
+		 nums2[] = nums[];
+		 System.out.print(nums[1]);
+		 System.out.print(nums2[1]);
+		 
+	 }
+}
